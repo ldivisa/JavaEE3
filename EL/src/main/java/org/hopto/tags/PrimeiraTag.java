51ed404a944f3069cca2aaf28feab491ef7429a1
luiz.souza <luiz.souza@DTI-12008.TJM.NET>
2025-02-03 20:35:33 -0300

    2025 02 03 A

---
+++ EL/src/main/java/org/hopto/tags/PrimeiraTag.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.hopto.tags;
+
+import jakarta.servlet.jsp.JspWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import jakarta.servlet.jsp.tagext.TagSupport;
+
+/**
+ *
+ * @author luiz.souza
+ */
+
+public class PrimeiraTag extends TagSupport {
+    @Override
+        public int doStartTag(){
+            JspWriter jspWriter = pageContext.getOut();
+            String imprimir = "Teste de saída via tld";
+        try {
+            jspWriter.println(imprimir);
+        } catch (IOException ex) {
+            Logger.getLogger(PrimeiraTag.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return SKIP_BODY;
+        }
+}
