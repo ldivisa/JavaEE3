1198e4c4c65cfb5f636a0820c3655773af0d8a6a
luiz <usocomercial@gmail.com>
2025-02-09 14:09:54 -0300

    2025 02 09 A

---
+++ EL/src/main/java/dataHorasemTags.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author luiz
+ */
+public class dataHorasemTags {
+    String[] diaSemana={"Domingo","Segunda", "Terça","Quarta","Quinta","Sexta","Sábado"};
+    String[] mesAno={"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+    
+}
