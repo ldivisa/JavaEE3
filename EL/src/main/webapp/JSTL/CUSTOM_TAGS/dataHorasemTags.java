d170b06f85e8a7918f8e044ed6ab4946a76b442d
luiz <usocomercial@gmail.com>
2025-02-09 14:18:21 -0300

    2025 02 09 A

---
+++ EL/src/main/webapp/JSTL/CUSTOM_TAGS/dataHorasemTags.java
@@ -0,0 +1,43 @@
+
+import java.util.Calendar;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author luiz
+ */
+public class dataHorasemTags {
+    private String[] diaSemana={"Domingo","Segunda", "Terça","Quarta","Quinta","Sexta","Sábado"};
+    private String[] mesAno={"Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"};
+    Calendar calendar = Calendar.getInstance();
+    private String hoje= calendar.getTime().toString();
+
+    public String[] getDiaSemana() {
+        return diaSemana;
+    }
+
+    public void setDiaSemana(String[] diaSemana) {
+        this.diaSemana = diaSemana;
+    }
+
+    public String[] getMesAno() {
+        return mesAno;
+    }
+
+    public void setMesAno(String[] mesAno) {
+        this.mesAno = mesAno;
+    }
+
+    public String getHoje() {
+        return hoje;
+    }
+
+    public void setHoje(String hoje) {
+        this.hoje = hoje;
+    }
+    
+}
