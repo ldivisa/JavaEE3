a556cd0e35b1013ec04e00681a3602e26c5a517b
Loja <Loja@SALASJRP>
2025-02-22 12:44:46 -0300

    20250222 - TESTE

--- EL/src/main/java/org/hopto/depositodivisa/servlets/ServletVariavisEscopo.java
+++ EL/src/main/java/org/hopto/depositodivisa/servlets/ServletVariavisEscopo.java
@@ -4,16 +4,11 @@
  */
 package org.hopto.depositodivisa.servlets;
 
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.util.Date;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
